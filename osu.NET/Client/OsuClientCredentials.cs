4a2033caedd3cb1d8fdddd2dfa2367ca3bc34e5f
VolcanicArts <VolcanicArtsBP@Gmail.com>
2022-01-05 22:53:52 +0000

    Make sure client credentials always exist

--- osu.NET/Client/OsuClientCredentials.cs
+++ osu.NET/Client/OsuClientCredentials.cs
@@ -6,18 +6,22 @@ namespace volcanicarts.osu.NET.Client
     public class OsuClientCredentials
     {
         [JsonProperty("grant_type")]
+        [NotNull]
         public const string GrantType = "client_credentials";
 
         [JsonProperty("scope")]
+        [NotNull]
         public const string Scope = "public";
 
         [JsonProperty("client_id")]
+        [NotNull]
         public string ClientId { get; private set; }
-
+        
         [JsonProperty("client_secret")]
+        [NotNull]
         public string ClientSecret { get; private set; }
 
-        public OsuClientCredentials([NotNull] string clientId, [NotNull] string clientSecret)
+        public OsuClientCredentials([NotNull] string clientId = "", [NotNull] string clientSecret = "")
         {
             ClientId = clientId;
             ClientSecret = clientSecret;
