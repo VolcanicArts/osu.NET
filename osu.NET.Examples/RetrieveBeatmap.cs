9f488330b7894dcd049e4989236197b2760e4478
VolcanicArts <VolcanicArtsBP@Gmail.com>
2022-04-30 21:53:57 +0100

    Add get beatmap example

---
+++ osu.NET.Examples/RetrieveBeatmap.cs
@@ -0,0 +1,23 @@
+﻿using volcanicarts.osu.NET.Client;
+
+var clientId = string.Empty; // your client id here
+var clientSecret = string.Empty; // your client secret here
+var osuClientCredentials = new OsuClientCredentials(clientId, clientSecret);
+
+var osuClient = new OsuClient(osuClientCredentials);
+await osuClient.LoginAsync();
+
+var beatmapId = string.Empty; // your beatmap Id here
+
+// method one
+var beatmapOne = await osuClient.GetBeatmapAsync(beatmapId);
+if (beatmapOne != null)
+{
+    Console.WriteLine(beatmapOne.Id);
+}
+
+// method two
+if (osuClient.TryGetBeatmap(beatmapId, out var beatmapTwo))
+{
+    Console.WriteLine(beatmapTwo.Id);
+}
